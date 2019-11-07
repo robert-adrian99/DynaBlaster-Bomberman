a63c892cd9eac9eac58d8db7244184d37322c108
Burtan Gabriela <burtangabriela@gmail.com>
2019-11-07 11:44:36 +0200

    Updated map by adding random walls. Changed the map design using ASCII Extended Codes.

--- DynaBlaster/Map.h
+++ DynaBlaster/Map.h
@@ -1,16 +1,19 @@
 #pragma once
-#include <vector>
-#include <iostream>
+#include <random>
 
 class Map
 {
 public:
-	Map(uint16_t linesNumber = 13, uint16_t columnsNumber = 15);
+	Map(uint16_t linesNumber = 13, uint16_t columnsNumber = 17);
 	friend std::ostream& operator <<(std::ostream& out, const Map& map);
 	uint16_t getLinesNumber() const;
 	uint16_t getColumnsNumber() const;
 
-private:
+protected:
+	int randomColumn(int random);
+	int randomLine(int random);
+
+protected:
 	uint16_t m_linesNumber, m_columnsNumber;
 	std::vector<std::vector<char>> m_map;
 };
