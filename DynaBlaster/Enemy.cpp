a75ad582fbd55fc0496d58cce2d95a3e7794bae8
Bogdan Gheorghe <56198430+DodeBogdan@users.noreply.github.com>
2019-11-14 20:23:28 +0200

    Modified Enemy class by adding structure binding.

--- DynaBlaster/Enemy.cpp
+++ DynaBlaster/Enemy.cpp
@@ -1,257 +1,337 @@
 #include "Enemy.h"
 #include <random>
+#include <vector>
 
-Enemy::Enemy(int xPos, int yPos, EnemyType enemyType)
+
+Enemy::Enemy(int xPosision, int yPosision, EnemyType enemyType)
 {
-	m_map[xPos][yPos] = 'X';
-	m_xPosition = xPos;
-	m_yPosition = yPos;
+	m_map[xPosision][yPosision] = 'X';
+	m_enemyPosition = std::make_pair(xPosision, yPosision);
 	m_enemyType = enemyType;
 }
-int Enemy::getEnemyPoints(EnemyType enemy)
+
+Enemy::Position Enemy::GetEnemyPosition() const
 {
-	switch (enemy)
-	{
-	case EnemyType::Barom:
-		return 100;
-	case EnemyType::Shashakin:
-		return 400;
-		break;
-	case EnemyType::Nagacham:
-		return 100;
-		break;
-	case EnemyType::Ojin:
-		return 400;
-		break;
-	case EnemyType::Pontan:
-		return 200;
-		break;
-	case EnemyType::Boyon:
-		return 1000;
-		break;
-	case EnemyType::Telpio:
-		return 1000;
-		break;
-	case EnemyType::Parce:
-		return 2000;
-		break;
-	case EnemyType::BigBoss:
-		return 20000;
-		break;
-	case EnemyType::Arion:
-		return 20000;
-		break;
-	default:
-		break;
-	}
+	return m_enemyPosition;
 }
 
-int randmovement(int number)
+uint16_t Enemy::GetX() const
+{
+	const auto& [xPosition, yPosition] = m_enemyPosition;
+	return xPosition;
+}
+
+uint16_t Enemy::GetY() const
+{
+	const auto& [xPosition, yPosition] = m_enemyPosition;
+	return yPosition;
+}
+
+int Enemy::RandomMovement(int number)
 {
 	std::random_device dev;
 	std::mt19937 rng(dev());
 	std::uniform_int_distribution<std::mt19937::result_type> rrandom(0, number);
 	return rrandom(rng);
 }
-void Enemy::enemyMovement(Enemy& enemy)
+
+void Enemy::Move()
 {
+	auto [xPosition, yPosition] = m_enemyPosition;
 	switch (m_enemyType)
 	{
 	case EnemyType::Barom:
 	{
-		char N = m_map[enemy.m_xPosition--][enemy.m_yPosition], S = m_map[enemy.m_xPosition++][enemy.m_yPosition],
-			E = m_map[enemy.m_xPosition][enemy.m_yPosition++], V = m_map[enemy.m_xPosition][enemy.m_yPosition--];
+		char N = m_map[xPosition - 1][yPosition];
+		char S = m_map[xPosition + 1][yPosition];
+		char E = m_map[xPosition][yPosition + 1];
+		char V = m_map[xPosition][yPosition - 1];
 		std::vector<char> posiblePos;
 		if (N == ' ') posiblePos.push_back(N);
 		if (S == ' ') posiblePos.push_back(S);
 		if (E == ' ') posiblePos.push_back(E);
 		if (V == ' ') posiblePos.push_back(V);
-		int ch = randmovement(posiblePos.size());
-		switch (ch)
+		int move = RandomMovement(posiblePos.size());
+		switch (move)
 		{
 		case 0:
-			enemy.m_xPosition--;
+			--xPosition;
 			break;
 		case 1:
-			enemy.m_xPosition++;
+			++xPosition;
 			break;
 		case 2:
-			enemy.m_yPosition++;
+			++yPosition;
 			break;
 		case 3:
-			enemy.m_yPosition--;
+			--yPosition;
+			break;
+		default:
 			break;
 		}
+		m_enemyPosition = std::make_pair(xPosition, yPosition);
 		break;
 	}
 	case EnemyType::Shashakin:
 	{
-		char N = m_map[enemy.m_xPosition--][enemy.m_yPosition], S = m_map[enemy.m_xPosition++][enemy.m_yPosition],
-			E = m_map[enemy.m_xPosition][enemy.m_yPosition++], V = m_map[enemy.m_xPosition][enemy.m_yPosition--];
+		char N = m_map[xPosition - 1][yPosition];
+		char S = m_map[xPosition + 1][yPosition];
+		char E = m_map[xPosition][yPosition + 1];
+		char V = m_map[xPosition][yPosition - 1];
 		std::vector<char> posiblePos;
 		if (N == ' ') posiblePos.push_back(N);
 		if (S == ' ') posiblePos.push_back(S);
 		if (E == ' ') posiblePos.push_back(E);
 		if (V == ' ') posiblePos.push_back(V);
-		int ch = randmovement(posiblePos.size());
-		switch (ch)
+		int move = RandomMovement(posiblePos.size());
+		switch (move)
 		{
 		case 0:
-			enemy.m_xPosition--;
+			--xPosition;
 			break;
 		case 1:
-			enemy.m_xPosition++;
+			++xPosition;
 			break;
 		case 2:
-			enemy.m_yPosition++;
+			++yPosition;
 			break;
 		case 3:
-			enemy.m_yPosition--;
+			--yPosition;
+			break;
+		default:
 			break;
 		}
+		m_enemyPosition = std::make_pair(xPosition, yPosition);
 		break;
 	}
 	case EnemyType::Nagacham:
-		break;
-	case EnemyType::Ojin:
 	{
-		char N = m_map[enemy.m_xPosition--][enemy.m_yPosition], S = m_map[enemy.m_xPosition++][enemy.m_yPosition],
-			E = m_map[enemy.m_xPosition][enemy.m_yPosition++], V = m_map[enemy.m_xPosition][enemy.m_yPosition--];
+		char N = m_map[xPosition - 1][yPosition];
+		char S = m_map[xPosition + 1][yPosition];
+		char E = m_map[xPosition][yPosition + 1];
+		char V = m_map[xPosition][yPosition - 1];
 		std::vector<char> posiblePos;
 		if (N == ' ') posiblePos.push_back(N);
 		if (S == ' ') posiblePos.push_back(S);
 		if (E == ' ') posiblePos.push_back(E);
 		if (V == ' ') posiblePos.push_back(V);
-		int ch = randmovement(posiblePos.size());
-		switch (ch)
+		int move = RandomMovement(posiblePos.size());
+		switch (move)
 		{
 		case 0:
-			enemy.m_xPosition--;
+			--xPosition;
 			break;
 		case 1:
-			enemy.m_xPosition++;
+			++xPosition;
 			break;
 		case 2:
-			enemy.m_yPosition++;
+			++yPosition;
 			break;
 		case 3:
-			enemy.m_yPosition--;
+			--yPosition;
+			break;
+		default:
 			break;
 		}
+		m_enemyPosition = std::make_pair(xPosition, yPosition);
+		break;
+	}
+	case EnemyType::Ojin:
+	{
+		char N = m_map[xPosition - 1][yPosition];
+		char S = m_map[xPosition + 1][yPosition];
+		char E = m_map[xPosition][yPosition + 1];
+		char V = m_map[xPosition][yPosition - 1];
+		std::vector<char> posiblePos;
+		if (N == ' ') posiblePos.push_back(N);
+		if (S == ' ') posiblePos.push_back(S);
+		if (E == ' ') posiblePos.push_back(E);
+		if (V == ' ') posiblePos.push_back(V);
+		int move = RandomMovement(posiblePos.size());
+		switch (move)
+		{
+		case 0:
+			--xPosition;
+			break;
+		case 1:
+			++xPosition;
+			break;
+		case 2:
+			++yPosition;
+			break;
+		case 3:
+			--yPosition;
+			break;
+		default:
+			break;
+		}
+		m_enemyPosition = std::make_pair(xPosition, yPosition);
 		break;
 	}
 	case EnemyType::Pontan:
 	{
-		char N = m_map[enemy.m_xPosition--][enemy.m_yPosition], S = m_map[enemy.m_xPosition++][enemy.m_yPosition],
-			E = m_map[enemy.m_xPosition][enemy.m_yPosition++], V = m_map[enemy.m_xPosition][enemy.m_yPosition--];
+		char N = m_map[xPosition - 1][yPosition];
+		char S = m_map[xPosition + 1][yPosition];
+		char E = m_map[xPosition][yPosition + 1];
+		char V = m_map[xPosition][yPosition - 1];
 		std::vector<char> posiblePos;
 		if (N == ' ') posiblePos.push_back(N);
 		if (S == ' ') posiblePos.push_back(S);
 		if (E == ' ') posiblePos.push_back(E);
 		if (V == ' ') posiblePos.push_back(V);
-		int ch = randmovement(posiblePos.size());
-		switch (ch)
+		int move = RandomMovement(posiblePos.size());
+		switch (move)
 		{
 		case 0:
-			enemy.m_xPosition--;
+			--xPosition;
 			break;
 		case 1:
-			enemy.m_xPosition++;
+			++xPosition;
 			break;
 		case 2:
-			enemy.m_yPosition++;
+			++yPosition;
 			break;
 		case 3:
-			enemy.m_yPosition--;
+			--yPosition;
+			break;
+		default:
 			break;
 		}
+		m_enemyPosition = std::make_pair(xPosition, yPosition);
 		break;
 	}
 	case EnemyType::Boyon:
 	{
-		char N = m_map[enemy.m_xPosition--][enemy.m_yPosition], S = m_map[enemy.m_xPosition++][enemy.m_yPosition],
-			E = m_map[enemy.m_xPosition][enemy.m_yPosition++], V = m_map[enemy.m_xPosition][enemy.m_yPosition--];
+		char N = m_map[xPosition - 1][yPosition];
+		char S = m_map[xPosition + 1][yPosition];
+		char E = m_map[xPosition][yPosition + 1];
+		char V = m_map[xPosition][yPosition - 1];
 		std::vector<char> posiblePos;
 		if (N == ' ') posiblePos.push_back(N);
 		if (S == ' ') posiblePos.push_back(S);
 		if (E == ' ') posiblePos.push_back(E);
 		if (V == ' ') posiblePos.push_back(V);
-		int ch = randmovement(posiblePos.size());
-		switch (ch)
+		int move = RandomMovement(posiblePos.size());
+		switch (move)
 		{
 		case 0:
-			enemy.m_xPosition--;
+			--xPosition;
 			break;
 		case 1:
-			enemy.m_xPosition++;
+			++xPosition;
 			break;
 		case 2:
-			enemy.m_yPosition++;
+			++yPosition;
 			break;
 		case 3:
-			enemy.m_yPosition--;
+			--yPosition;
+			break;
+		default:
 			break;
 		}
+		m_enemyPosition = std::make_pair(xPosition, yPosition);
 		break;
 	}
 	case EnemyType::Telpio:
 	{
-		char N = m_map[enemy.m_xPosition--][enemy.m_yPosition], S = m_map[enemy.m_xPosition++][enemy.m_yPosition],
-			E = m_map[enemy.m_xPosition][enemy.m_yPosition++], V = m_map[enemy.m_xPosition][enemy.m_yPosition--];
+		char N = m_map[xPosition - 1][yPosition];
+		char S = m_map[xPosition + 1][yPosition];
+		char E = m_map[xPosition][yPosition + 1];
+		char V = m_map[xPosition][yPosition - 1];
 		std::vector<char> posiblePos;
-		if (N == ' ') posiblePos.push_back(N);
-		if (S == ' ') posiblePos.push_back(S);
-		if (E == ' ') posiblePos.push_back(E);
-		if (V == ' ') posiblePos.push_back(V);
-		int ch = randmovement(posiblePos.size());
-		switch (ch)
+		if (N != (char)'254') posiblePos.push_back(N);
+		if (S != (char)'254') posiblePos.push_back(S);
+		if (E != (char)'254') posiblePos.push_back(E);
+		if (V != (char)'254') posiblePos.push_back(V);
+		int move = RandomMovement(posiblePos.size());
+		switch (move)
 		{
 		case 0:
-			enemy.m_xPosition--;
+			--xPosition;
 			break;
 		case 1:
-			enemy.m_xPosition++;
+			++xPosition;
 			break;
 		case 2:
-			enemy.m_yPosition++;
+			++yPosition;
 			break;
 		case 3:
-			enemy.m_yPosition--;
+			--yPosition;
+			break;
+		default:
 			break;
 		}
+		m_enemyPosition = std::make_pair(xPosition, yPosition);
 		break;
 	}
 	case EnemyType::Parce:
 	{
-		char N = m_map[enemy.m_xPosition--][enemy.m_yPosition], S = m_map[enemy.m_xPosition++][enemy.m_yPosition],
-			E = m_map[enemy.m_xPosition][enemy.m_yPosition++], V = m_map[enemy.m_xPosition][enemy.m_yPosition--];
+		char N = m_map[xPosition - 1][yPosition];
+		char S = m_map[xPosition + 1][yPosition];
+		char E = m_map[xPosition][yPosition + 1];
+		char V = m_map[xPosition][yPosition - 1];
 		std::vector<char> posiblePos;
 		if (N == ' ') posiblePos.push_back(N);
 		if (S == ' ') posiblePos.push_back(S);
 		if (E == ' ') posiblePos.push_back(E);
 		if (V == ' ') posiblePos.push_back(V);
-		int ch = randmovement(posiblePos.size());
-		switch (ch)
+		int move = RandomMovement(posiblePos.size());
+		switch (move)
 		{
 		case 0:
-			enemy.m_xPosition--;
+			--xPosition;
 			break;
 		case 1:
-			enemy.m_xPosition++;
+			++xPosition;
 			break;
 		case 2:
-			enemy.m_yPosition++;
+			++yPosition;
 			break;
 		case 3:
-			enemy.m_yPosition--;
+			--yPosition;
+			break;
+		default:
 			break;
 		}
+		m_enemyPosition = std::make_pair(xPosition, yPosition);
 		break;
 	}
 	case EnemyType::BigBoss:
 		break;
 	case EnemyType::Arion:
 		break;
+	default:
+		break;
+	}
+}
+
+uint32_t Enemy::Die()
+{
+	m_enemyPosition = std::make_pair(0, 0);
+	m_map[0][0] = (char)219;
+
+	switch (m_enemyType)
+	{
+	case EnemyType::Barom:
+		return 100;
+	case EnemyType::Shashakin:
+		return 400;
+	case EnemyType::Nagacham:
+		return 100;
+	case EnemyType::Ojin:
+		return 400;
+	case EnemyType::Pontan:
+		return 200;
+	case EnemyType::Boyon:
+		return 1000;
+	case EnemyType::Telpio:
+		return 1000;
+	case EnemyType::Parce:
+		return 2000;
+	case EnemyType::BigBoss:
+		return 20000;
+	case EnemyType::Arion:
+		return 20000;
 	default:
 		break;
 	}
--- DynaBlaster/Enemy.h
+++ DynaBlaster/Enemy.h
@@ -3,17 +3,22 @@
 #include <cstdint>
 #include "Map.h"
 #include "EnemyType.h"
+
 class Enemy : Map
 {
 public:
-	Enemy(int xPos, int yPosm, EnemyType enemyType);
-	uint16_t getX() { return m_xPosition; }
-	uint16_t getY() { return m_yPosition; }
-	void enemyMovement(Enemy& enemy);
-	int getEnemyPoints(EnemyType enemy);
+	Enemy(int xPosision, int yPosision, EnemyType enemyType);
+	Position GetEnemyPosition() const;
+	uint16_t GetX() const;
+	uint16_t GetY() const;
+	void Move();
+	uint32_t Die();
+
 private:
-	uint16_t m_xPosition;
-	uint16_t m_yPosition;
+	int RandomMovement(int number);
+
+private:
+	Position m_enemyPosition;
 	EnemyType m_enemyType;
 };
 
