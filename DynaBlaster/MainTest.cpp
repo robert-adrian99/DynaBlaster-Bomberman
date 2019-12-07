f43b9cd8c598e05c311344ce318dfd6e0f05ff86
Bogdan Gheorghe <56198430+DodeBogdan@users.noreply.github.com>
2019-12-08 00:28:18 +0200

    Added map and a method that update my location on the window.

--- DynaBlaster/MainTest.cpp
+++ DynaBlaster/MainTest.cpp
@@ -9,63 +9,95 @@ void MainTest::Execute()
 	class Player
 	{
 	private:
-		float frameCounter = 0, switchFrame = 100, frameSpeed = 500;
-		bool updateFrame = true;
 		enum Direction { Up = 8, Left, Down, Right };
 		sf::Vector2i source = { 0, Up };
 		sf::Texture pTexture;
 		sf::Sprite playerImage;
-		sf::Clock clock;
 		float bottom, left, right, top;
 		bool ok = true;
 	public:
+		sf::RectangleShape rect;
 		Player()
 		{
-			if (!pTexture.loadFromFile("TestPlayer.png"))
+			if (!pTexture.loadFromFile("TestPlayer.png", { 1 * 64,64,64,64 }))
 				std::cout << "Error" << std::endl;
 			playerImage.setTexture(pTexture);
-			playerImage.setScale(sf::Vector2f(0.5, 0.5));
-			playerImage.setPosition(30, 30);
-		}
-
-		sf::Sprite GetSprite()
-		{
-			return playerImage;
+			rect.setTexture(playerImage.getTexture());
+			rect.setSize({ 32,32 });
+			rect.setFillColor(sf::Color::White);
+			rect.setOutlineColor(sf::Color::Black);
+			rect.setOutlineThickness(1.0f);
+			top = rect.getPosition().y;
+			left = rect.getPosition().x;
+			bottom = rect.getPosition().y + 64;
+			right = rect.getPosition().x + 64;
+			std::cout << "TLBR: " << " " << top << " " << left << " " << bottom << " " << right << "\n\n\n";
 		}
 		void Movement()
 		{
 			float speed = 0.2;
 			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
 				source.y = Up;
-				//std::cout << "UP";
-				playerImage.move(0, -speed);
+				rect.move(0, -speed);
 			}
 			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 			{
 				source.y = Down;
-				//std::cout << "Down";
-				playerImage.move(0, speed);
+				rect.move(0, speed);
 			}
 			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 			{
 				source.y = Right;
-				//std::cout << "Right";
-				playerImage.move(speed, 0);
+				rect.move(speed, 0);
 			}
 			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::A))
 			{
 				source.y = Left;
-				//std::cout << "Left";
-				playerImage.move(-speed, 0);
+				rect.move(-speed, 0);
 			}
-			playerImage.setTextureRect(sf::IntRect(source.x * 64, source.y * 64, 64, 64));
+			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::C))
+			{
+				std::cout << "TLBR: " << " " << top << " " << left << " " << bottom << " " << right << "\n\n\n";
 
+			}
+			//playerImage.setTextureRect(sf::IntRect(source.x * 64, source.y * 64, 64, 64));
+			//rect.setTexture(playerImage.getTexture());
+
+		}
+		void Update()
+		{
+			top = rect.getPosition().y;
+			left = rect.getPosition().x;
+			bottom = rect.getPosition().y + 32;
+			right = rect.getPosition().x + 32;
 		}
 	};
 	Player player;
 	sf::RenderWindow Window;
 	Window.create(sf::VideoMode(800, 600), "Test SFML");
 	Window.setKeyRepeatEnabled(true);
+
+	const int level[] =
+	{
+		3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+		3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+		3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3,
+		3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+		3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3,
+		3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+		3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3,
+		3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+		3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3,
+		3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+		3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3, 0, 3,
+		3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3,
+		3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3
+	};
+	// create the tilemap from the level definition
+	TileMap map;
+	if (!map.load("tileset.png", sf::Vector2u(32, 32), level, 15, 13))
+		std::cout << "Error" << std::endl;
+
 
 	while (Window.isOpen())
 	{
@@ -84,7 +116,8 @@ void MainTest::Execute()
 			}
 		}
 		player.Movement();
-		Window.draw(player.GetSprite());
+		Window.draw(map);
+		Window.draw(player.rect);
 		Window.display();
 		Window.clear();
 	}
