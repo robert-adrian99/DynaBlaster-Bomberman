d21b178ba062790226a069c0b27ced5e4241184d
Bogdan Gheorghe <56198430+DodeBogdan@users.noreply.github.com>
2019-12-13 00:13:13 +0200

    Add testing and two new method for random create walls in MainTest.

--- DynaBlasterTests/EnemyTests.cpp
+++ DynaBlasterTests/EnemyTests.cpp
@@ -18,5 +18,43 @@ namespace DynaBlasterTests
 			Assert::IsTrue(enemy.GetX() == 0);
 			Assert::IsTrue(enemy.GetY() == 0);
 		}
+		TEST_METHOD(Die)
+		{
+			Map map;
+			std::vector<Enemy> enemies{
+			(0,0,EnemyType::Barom, map),
+			(0,0,EnemyType::Shashakin, map),
+			(0,0,EnemyType::Nagacham, map),
+			(0,0,EnemyType::Ojin, map),
+			(0,0,EnemyType::Pontan, map),
+			(0,0,EnemyType::Boyon, map),
+			(0,0,EnemyType::Telpio, map),
+			(0,0,EnemyType::Parce, map),
+			(0,0,EnemyType::BigBoss, map),
+			(0,0,EnemyType::Arion, map),
+			};
+			std::vector<int> points{
+			100,
+			400,
+			100,
+			400,
+			200,
+			1000,
+			1000,
+			2000,
+			20000,
+			20000,
+			};
+			Assert::IsTrue(enemies[0].Die(map) == points[0]);
+			Assert::IsTrue(enemies[1].Die(map) == points[1]);
+			Assert::IsTrue(enemies[2].Die(map) == points[2]);
+			Assert::IsTrue(enemies[3].Die(map) == points[3]);
+			Assert::IsTrue(enemies[4].Die(map) == points[4]);
+			Assert::IsTrue(enemies[5].Die(map) == points[5]);
+			Assert::IsTrue(enemies[6].Die(map) == points[6]);
+			Assert::IsTrue(enemies[7].Die(map) == points[7]);
+			Assert::IsTrue(enemies[8].Die(map) == points[8]);
+			Assert::IsTrue(enemies[9].Die(map) == points[9]);
+		}
 	};
 }
